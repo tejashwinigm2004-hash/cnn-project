0b48f9c8e436235e507c8da68ef23d46f3c23751
Narasimha Murthy C N <nnarasimhamurthycn@gmail.com>
2026-05-03 13:11:31 +0530

    error fixed

--- server/models/User.js
+++ server/models/User.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema({
+const UserSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
@@ -9,4 +9,4 @@ const userSchema = new mongoose.Schema({
   role: { type: String, default: 'user' }
 }, { timestamps: true });
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model('User', UserSchema);
