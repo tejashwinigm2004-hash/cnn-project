3f5616c554f8713ec113e7063045b167aee9c33d
Narasimha Murthy C N <nnarasimhamurthycn@gmail.com>
2026-07-28 10:31:31 +0530

    Load Firebase credentials from env var on Render

--- server/utils/firebaseAdmin.js
+++ server/utils/firebaseAdmin.js
@@ -1,7 +1,7 @@
 const admin = require('firebase-admin');
- 
+
 let serviceAccount;
- 
+
 if (process.env.FIREBASE_SERVICE_ACCOUNT) {
   // Production (Render): credentials stored as a JSON string in an env var
   serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
@@ -9,11 +9,11 @@ if (process.env.FIREBASE_SERVICE_ACCOUNT) {
   // Local dev: fall back to the local file (kept out of git via .gitignore)
   serviceAccount = require('../firebase-service-account.json');
 }
- 
+
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
   });
 }
- 
+
 module.exports = admin;
